3e87e4a4fedf2a0973a7a0b80fcfde4a022e09bd
swksysb114 <swksysb1124@gmail.com>
2024-03-02 09:55:29 +0800

    feat: show LessonScreen

--- app/src/main/java/com/example/myfitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/myfitnessapp/MainActivity.kt
@@ -4,21 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.ViewModelProvider
-import com.example.myfitnessapp.ui.screen.login.LoginScreen
+import com.example.myfitnessapp.ui.screen.lession.LessonScreen
+import com.example.myfitnessapp.ui.screen.lession.LessonViewModel
 import com.example.myfitnessapp.ui.screen.login.LoginViewModel
 import com.example.myfitnessapp.ui.theme.MyFitnessAppTheme
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var loginViewModel: LoginViewModel
+    private lateinit var lessonViewModel: LessonViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         loginViewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+        lessonViewModel = ViewModelProvider(this)[LessonViewModel::class.java]
 
         setContent {
             MyFitnessAppTheme {
-                LoginScreen(loginViewModel)
+                LessonScreen(viewModel = lessonViewModel)
             }
         }
     }
