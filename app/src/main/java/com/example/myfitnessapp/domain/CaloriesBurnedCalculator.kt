cc6fada5a11e07ac0b815124b83b767d46f2feb4
swksysb114 <swksysb1124@gmail.com>
2024-03-08 00:40:30 +0800

    refactor: remove Config in CaloriesBurnedCalculator and make Mets as data class

--- app/src/main/java/com/example/myfitnessapp/domain/CaloriesBurnedCalculator.kt
+++ app/src/main/java/com/example/myfitnessapp/domain/CaloriesBurnedCalculator.kt
@@ -8,17 +8,11 @@ class CaloriesBurnedCalculator {
      * @param weightInKg: the weight in KG
      * @param mins: Minutes of the exercise
      */
-    private val caloriesBurnedFormula: (mets: Mets, weightInKg: Int, mins: Int) -> Float =
+    private val caloriesBurnedFormula: (mets: Mets, weightInKg: Double, mins: Double) -> Double =
         { mets, weight, mins -> mets.value * 3.5f * weight / 200 * mins }
 
-    fun calculate(config: Config) =
-        caloriesBurnedFormula(config.mets, config.weightInKg, config.mins)
-
-    data class Config(
-        val mets: Mets,
-        val weightInKg: Int,
-        val mins: Int
-    )
+    fun calculate(mets: Mets, weightInKg: Double, mins: Double) =
+        caloriesBurnedFormula(mets, weightInKg, mins)
 }
 
 /**
@@ -26,15 +20,4 @@ class CaloriesBurnedCalculator {
  *
  * Reference: https://onlinelibrary.wiley.com/doi/pdf/10.1002/clc.4960130809
  */
-sealed class Mets(val value: Float)
-data object LowStrengthRunning : Mets(8.0f)
-data object MediumStrengthRunning : Mets(11.5f)
-data object HighStrengthRunning : Mets(16.0f)
-data object BrisklyWalking : Mets(4.5f)
-data object Swimming : Mets(7.0f)
-data object PushUp : Mets(3.8f)
-data object PullUp : Mets(3.8f)
-data object Squat : Mets(3.8f)
-data object Plank : Mets(3.8f)
-data object Crunch : Mets(3.8f)
-data object Bridge : Mets(3.8f)
+data class Mets(val value: Float)
