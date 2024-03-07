469971e5fb4b1b5873cae87b231554ed05d01cbc
swksysb114 <swksysb1124@gmail.com>
2024-03-08 00:45:25 +0800

    refactor: refine unit in DurationUtil.kt

--- app/src/main/java/com/example/myfitnessapp/util/DurationUtil.kt
+++ app/src/main/java/com/example/myfitnessapp/util/DurationUtil.kt
@@ -1,17 +1,17 @@
 package com.example.myfitnessapp.util
 
 /**
- * Covert duration in millis second to the format HH:mm:ss
+ * Covert duration in second to the format HH:mm:ss
  *
  * For example:
- *   - 10,000ms -> 00:10
- *   - 60,000ms -> 01:00
- *   - 70,000ms -> 01:10
- *   - 3,600,000,000 -> 1:00:00
- *   - 3,600,070,000 -> 1:01:10
+ *   - 10s -> 00:10
+ *   - 60s -> 01:00
+ *   - 70s -> 01:10
+ *   - 3,600s -> 1:00:00
+ *   - 3,670s -> 1:01:10
  */
-fun Long.formattedDuration(): String {
-    val seconds = this / 1000
+fun Int.formattedDuration(): String {
+    val seconds = this
     val hour = seconds / 3600
     val minute = (seconds % 3600) / 60
     val secondLeft = (seconds % 60)
@@ -23,18 +23,18 @@ fun Long.formattedDuration(): String {
 }
 
 /**
- * Covert duration in millis second to a speakable sentence in Chinese
+ * Covert duration in second to a speakable sentence in Chinese
  *
  * For example:
- *   - 10,000ms -> 10秒
- *   - 60,000ms -> 1分鐘
- *   - 70,000ms -> 1分鐘10秒
- *   - 3,600,000,000 -> 1小時
- *   - 3,600,070,000 -> 1小時1分鐘10秒
+ *   - 10s -> 10秒
+ *   - 60s -> 1分鐘
+ *   - 70s -> 1分鐘10秒
+ *   - 3,600s -> 1小時
+ *   - 3,670s -> 1小時1分鐘10秒
  */
-fun Long.speakableDuration(): String {
-    if (this == 0L) return ""
-    val seconds = this / 1000
+fun Int.speakableDuration(): String {
+    if (this == 0) return ""
+    val seconds = this
     val hour = seconds / 3600
     val minute = (seconds % 3600) / 60
     val secondLeft = (seconds % 60)
