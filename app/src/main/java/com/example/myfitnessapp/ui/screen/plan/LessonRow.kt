9b8548740fc40d15949ce39c50afa4d64afa6ed4
swksysb114 <swksysb1124@gmail.com>
2024-03-09 10:05:47 +0800

    feat: implement MyPlanScreen and LessonRow

---
+++ app/src/main/java/com/example/myfitnessapp/ui/screen/plan/LessonRow.kt
@@ -0,0 +1,67 @@
+package com.example.myfitnessapp.ui.screen.plan
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myfitnessapp.ui.screen.rowBackgroundColor
+import com.example.myfitnessapp.ui.screen.textColor
+import com.example.myfitnessapp.ui.theme.MyFitnessAppTheme
+
+@Composable
+fun LessonRow(
+    modifier: Modifier = Modifier,
+    name: String? = null,
+    startTime: String,
+    duration: String,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(rowBackgroundColor, shape = RoundedCornerShape(20.dp))
+            .height(120.dp)
+            .padding(horizontal = 20.dp, vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            if (name != null) {
+                Text(text = name, fontSize = 16.sp, color = Color.DarkGray)
+            }
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(startTime, fontSize = 30.sp, color = textColor, textAlign = TextAlign.Center)
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(duration, fontSize = 16.sp, color = textColor)
+        }
+        Box {
+            Text("日 一 二 三 四 五 六", fontSize = 18.sp, color = Color.Gray)
+        }
+    }
+}
+
+@Preview(widthDp = 300)
+@Composable
+fun LessonRowPreview() {
+    MyFitnessAppTheme {
+        LessonRow(
+            name ="晨間運動",
+            startTime = "18:00",
+            duration = "一小時"
+        )
+    }
+}
---
+++ app/src/main/java/com/example/myfitnessapp/ui/screen/plan/MyPlanScreen.kt
@@ -0,0 +1,94 @@
+package com.example.myfitnessapp.ui.screen.plan
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myfitnessapp.ui.screen.backgroundColor
+import com.example.myfitnessapp.ui.screen.textColor
+import com.example.myfitnessapp.ui.theme.MyFitnessAppTheme
+
+@Composable
+fun MyPlanScreen() {
+    val lessons = listOf(
+        Lesson(name = "晨間運動", startTime = "07:00", duration = "一小時"),
+        Lesson(name = "夜間運動", startTime = " 18:00", duration = "30分鐘"),
+        Lesson(startTime = " 10:00", duration = "30分鐘")
+    )
+    Column(
+        Modifier
+            .background(backgroundColor)
+            .fillMaxSize()
+    ) {
+        ScreenTitleRow("我的訓練") {
+            IconButton(
+                onClick = { /*TODO*/ }) {
+                Icon(
+                    modifier = Modifier.size(25.dp),
+                    imageVector = Icons.Filled.Add, contentDescription = null, tint = Color.White
+                )
+            }
+        }
+        LazyColumn(
+            Modifier.padding(horizontal = 20.dp, vertical = 12.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(lessons) { lesson ->
+                LessonRow(
+                    name = lesson.name,
+                    startTime = lesson.startTime,
+                    duration = lesson.duration
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ScreenTitleRow(
+    title: String,
+    icon: @Composable () -> Unit
+) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 25.dp, vertical = 20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(title, fontSize = 25.sp, color = textColor)
+        icon()
+    }
+}
+
+data class Lesson(
+    val name: String? = null,
+    val startTime: String,
+    val duration: String,
+)
+
+@Preview
+@Composable
+fun MyPlanScreenPreview() {
+    MyFitnessAppTheme {
+        MyPlanScreen()
+    }
+}
