b1f8fbe5441fde910c6d55cf3170671d326711ae
swksysb114 <swksysb1124@gmail.com>
2024-03-10 22:14:15 +0800

    fix: fit typo of name of Crunch

--- app/src/main/java/com/example/myfitnessapp/model/Exercise.kt
+++ app/src/main/java/com/example/myfitnessapp/model/Exercise.kt
@@ -18,6 +18,6 @@ sealed class Exercise(
     data object PullUp : Exercise("引體向上", R.drawable.pull_up, Mets(3.8f))
     data object Squat : Exercise("深蹲", R.drawable.squat, Mets(3.8f))
     data object Plank : Exercise("棒式", R.drawable.plank, Mets(3.8f))
-    data object Crunch : Exercise("卷腹", R.drawable.crunch, Mets(3.8f))
+    data object Crunch : Exercise("捲腹", R.drawable.crunch, Mets(3.8f))
     data object Bridge : Exercise("橋式", R.drawable.bridge, Mets(3.8f))
 }
