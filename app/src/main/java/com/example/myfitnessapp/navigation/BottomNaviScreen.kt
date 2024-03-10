d90a92fd07865c2938f88dcad6bd41ab1dd791b2
swksysb114 <swksysb1124@gmail.com>
2024-03-10 09:51:12 +0800

    fix: address typo and name to BottomNaviScreen

--- app/src/main/java/com/example/myfitnessapp/navigation/BottomNaviScreen.kt
+++ app/src/main/java/com/example/myfitnessapp/navigation/BottomNaviScreen.kt
@@ -5,14 +5,14 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed class BotoomeNaviScreen(val route: String, val name: String, val icon: ImageVector) {
-    data object MyLesson : BotoomeNaviScreen(
+sealed class BottomNaviScreen(val route: String, val name: String, val icon: ImageVector) {
+    data object MyLesson : BottomNaviScreen(
         route = "my_lesson",
         name = "我的訓練",
         icon = Icons.Filled.DateRange
     )
 
-    data object Profile : BotoomeNaviScreen(
+    data object Profile : BottomNaviScreen(
         route = "profile",
         name = "我的檔案",
         icon = Icons.Filled.AccountCircle
