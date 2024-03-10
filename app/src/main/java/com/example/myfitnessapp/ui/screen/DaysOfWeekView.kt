4bfbf80e6ac0680885d200d5bcbfc05ed6880d7d
swksysb114 <swksysb1124@gmail.com>
2024-03-10 21:03:49 +0800

    feat: implement DaysOfWeekView

---
+++ app/src/main/java/com/example/myfitnessapp/ui/screen/DaysOfWeekView.kt
@@ -0,0 +1,43 @@
+package com.example.myfitnessapp.ui.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myfitnessapp.model.WeekDay
+
+@Composable
+fun DaysOfWeekView(daysOfWeek: Set<WeekDay>) {
+    Box {
+        val daysOfWeekArray = arrayOf(false, false, false, false, false, false, false)
+        daysOfWeek.forEach { day ->
+            daysOfWeekArray[day.ordinal] = true
+        }
+
+        Row {
+            daysOfWeekArray.forEachIndexed { index, isDaySelected ->
+                Text(
+                    WeekDay.entries[index].value,
+                    fontSize = 10.sp,
+                    color = if (isDaySelected) backgroundColor else textColor,
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .drawBehind {
+                            drawCircle(
+                                style = if (isDaySelected) Fill else Stroke(width = 1.dp.toPx()),
+                                color = textColor,
+                                radius = this.size.maxDimension
+                            )
+                        }
+                )
+            }
+        }
+    }
+}
