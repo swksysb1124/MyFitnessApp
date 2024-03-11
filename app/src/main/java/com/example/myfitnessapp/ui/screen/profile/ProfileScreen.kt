cc4d57801a28f623d5e9a2b2ab63cf7c029ff057
swksysb114 <swksysb1124@gmail.com>
2024-03-11 08:17:09 +0800

    feat: show dialog when editing and add arrow in ProfileInfoRow

--- app/src/main/java/com/example/myfitnessapp/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/myfitnessapp/ui/screen/profile/ProfileScreen.kt
@@ -1,13 +1,17 @@
 package com.example.myfitnessapp.ui.screen.profile
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowRight
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myfitnessapp.ui.color.backgroundColor
+import com.example.myfitnessapp.ui.color.buttonBackgroundColor
 import com.example.myfitnessapp.ui.color.textColor
 import com.example.myfitnessapp.ui.component.ScreenTitleRow
 
@@ -24,8 +29,14 @@ fun ProfileScreen(
     viewModel: ProfileViewModel,
     modifier: Modifier = Modifier
 ) {
-    val height by viewModel.heightInCm.observeAsState()
-    val weight by viewModel.weightInKg.observeAsState()
+    val height by viewModel.heightInCm.observeAsState(0.0)
+    val weight by viewModel.weightInKg.observeAsState(0.0)
+    val editDialogConfig by viewModel.editDialogConfig.observeAsState(
+        ProfileEditDialogConfig(
+            ProfileEditType.Height,
+            isDialogOpen = false
+        )
+    )
 
     Column(
         modifier = modifier
@@ -38,6 +49,12 @@ fun ProfileScreen(
             value = "${height}cm",
             modifier = Modifier
                 .fillMaxWidth()
+                .clickable {
+                    viewModel.openDialog(
+                        value = height.toString(),
+                        type = ProfileEditType.Height
+                    )
+                }
                 .padding(
                     horizontal = 20.dp,
                     vertical = 12.dp
@@ -49,6 +66,12 @@ fun ProfileScreen(
             value = "${weight}kg",
             modifier = Modifier
                 .fillMaxWidth()
+                .clickable {
+                    viewModel.openDialog(
+                        value = weight.toString(),
+                        type = ProfileEditType.Weight
+                    )
+                }
                 .padding(
                     horizontal = 20.dp,
                     vertical = 12.dp
@@ -56,6 +79,24 @@ fun ProfileScreen(
         )
         Divider(Modifier.padding(horizontal = 20.dp))
     }
+
+    if (editDialogConfig.isDialogOpen) {
+        ProfileInfoEditDialog(
+            type = editDialogConfig.type,
+            value = editDialogConfig.value,
+            onValueChanged = { type, value ->
+                viewModel.onSaved(type, value)
+                viewModel.closeDialog(type)
+            }
+        ) {
+            viewModel.closeDialog(editDialogConfig.type)
+        }
+    }
+}
+
+enum class ProfileEditType(val title: String) {
+    Height("身高 (單位: cm)"),
+    Weight("體重 (單位: kg)")
 }
 
 @Composable
@@ -69,6 +110,13 @@ private fun ProfileInfoRow(
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(label, color = textColor, fontSize = 20.sp)
-        Text(value, color = textColor, fontSize = 20.sp)
+        Row {
+            Text(value, color = buttonBackgroundColor, fontSize = 20.sp)
+            Icon(
+                imageVector = Icons.AutoMirrored.Outlined.KeyboardArrowRight,
+                contentDescription = null,
+                tint = buttonBackgroundColor
+            )
+        }
     }
 }
--- app/src/main/java/com/example/myfitnessapp/ui/screen/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/myfitnessapp/ui/screen/profile/ProfileViewModel.kt
@@ -10,11 +10,14 @@ import kotlinx.coroutines.launch
 class ProfileViewModel(
     private val profileRepository: ProfileRepository
 ) : ViewModel() {
-    private val _heightInCm = MutableLiveData<Double>()
-    val heightInCm: LiveData<Double> = _heightInCm
+    private val _heightInCm = MutableLiveData<Int>()
+    val heightInCm: LiveData<Int> = _heightInCm
 
-    private val _weightInKg = MutableLiveData<Double>()
-    val weightInKg: LiveData<Double> = _weightInKg
+    private val _weightInKg = MutableLiveData<Int>()
+    val weightInKg: LiveData<Int> = _weightInKg
+
+    private val _editDialogConfig = MutableLiveData<ProfileEditDialogConfig>()
+    val editDialogConfig: LiveData<ProfileEditDialogConfig> = _editDialogConfig
 
     init {
         viewModelScope.launch {
@@ -23,4 +26,48 @@ class ProfileViewModel(
             _weightInKg.value = profile.weight
         }
     }
+
+    fun openDialog(value: String, type: ProfileEditType) {
+        _editDialogConfig.value = ProfileEditDialogConfig(
+            type = type,
+            value = value,
+            isDialogOpen = true
+        )
+    }
+
+    fun closeDialog(type: ProfileEditType) {
+        _editDialogConfig.value = ProfileEditDialogConfig(
+            type = type,
+            isDialogOpen = false
+        )
+    }
+
+    fun onSaved(
+        type: ProfileEditType,
+        value: String
+    ) {
+        val updateValue = value.toIntOrNull() ?: 0
+        val profile = profileRepository.getProfile()
+        when (type) {
+            ProfileEditType.Height -> {
+                profileRepository.saveProfile(
+                    profile.copy(height = updateValue)
+                )
+                _heightInCm.value = updateValue
+            }
+
+            ProfileEditType.Weight -> {
+                profileRepository.saveProfile(
+                    profile.copy(weight = updateValue)
+                )
+                _weightInKg.value = updateValue
+            }
+        }
+    }
 }
+
+data class ProfileEditDialogConfig(
+    val type: ProfileEditType,
+    val value: String? = null,
+    val isDialogOpen: Boolean
+)
