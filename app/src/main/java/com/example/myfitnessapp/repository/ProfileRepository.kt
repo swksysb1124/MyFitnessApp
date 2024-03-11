0ea82857c327960f27ad8ebfcfa80ba19e28e472
swksysb114 <swksysb1124@gmail.com>
2024-03-11 08:12:50 +0800

    feat: implement profile cache and saveProfile

--- app/src/main/java/com/example/myfitnessapp/repository/ProfileRepository.kt
+++ app/src/main/java/com/example/myfitnessapp/repository/ProfileRepository.kt
@@ -3,10 +3,16 @@ package com.example.myfitnessapp.repository
 import com.example.myfitnessapp.model.Profile
 
 class ProfileRepository {
+    /**
+     * cached profile, default height = 169.5, default weight = 82.30
+     */
+    private var profile = Profile(height = 169, weight = 82)
+
     fun getProfile(): Profile {
-        return Profile(
-            height = 169.5,
-            weight = 82.30
-        )
+        return profile
+    }
+
+    fun saveProfile(newProfile: Profile) {
+        profile = newProfile
     }
 }
