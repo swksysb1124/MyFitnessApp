4b623b3766e81b00f613af76740d9e5bd102a189
swksysb114 <swksysb1124@gmail.com>
2024-03-12 08:24:13 +0800

    feat: update selectedExercises by AddLessonViewModel

--- app/src/main/java/com/example/myfitnessapp/ui/screen/lession/add/AddLessonViewModel.kt
+++ app/src/main/java/com/example/myfitnessapp/ui/screen/lession/add/AddLessonViewModel.kt
@@ -1,14 +1,15 @@
 package com.example.myfitnessapp.ui.screen.lession.add
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.myfitnessapp.model.Exercise
 import com.example.myfitnessapp.model.WeekDay
 
-class AddLessonViewModel: ViewModel() {
+class AddLessonViewModel : ViewModel() {
     private val _name = MutableLiveData<String>()
-    val name : LiveData<String> = _name
+    val name: LiveData<String> = _name
 
     private val _startTime = MutableLiveData<String>()
     val startTime: LiveData<String> = _startTime
@@ -16,8 +17,7 @@ class AddLessonViewModel: ViewModel() {
     private val _daysOfWeek = MutableLiveData<Set<WeekDay>>()
     val daysOfWeek: LiveData<Set<WeekDay>> = _daysOfWeek
 
-    private val _exercises = MutableLiveData<List<Exercise>>()
-    val exercises: LiveData<List<Exercise>> = _exercises
+    private val selectedExercises = mutableStateListOf<Exercise>()
 
     fun updateName(name: String) {
         _name.value = name
@@ -31,7 +31,15 @@ class AddLessonViewModel: ViewModel() {
         _daysOfWeek.value = daysOfWeek
     }
 
-    fun updateExercises(exercises: List<Exercise>) {
-        _exercises.value = exercises
+    fun isExerciseSelected(exercise: Exercise): Boolean {
+        return selectedExercises.contains(exercise)
+    }
+
+    fun updateExercises(selected: Boolean, exercise: Exercise) {
+        if (selected) {
+            selectedExercises.add(exercise)
+        } else {
+            selectedExercises.remove(exercise)
+        }
     }
 }
