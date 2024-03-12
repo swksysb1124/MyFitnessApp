f60f93c1ffc71918d420a7680c6143ff2256c6c3
swksysb114 <swksysb1124@gmail.com>
2024-03-12 22:20:02 +0800

    feat: enhance DaysOfWeekView by inject Modifier

--- app/src/main/java/com/example/myfitnessapp/ui/component/DaysOfWeekView.kt
+++ app/src/main/java/com/example/myfitnessapp/ui/component/DaysOfWeekView.kt
@@ -1,7 +1,9 @@
 package com.example.myfitnessapp.ui.component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,22 +13,29 @@ import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.myfitnessapp.model.WeekDay
+import com.example.myfitnessapp.model.DayOfWeek
 import com.example.myfitnessapp.ui.color.backgroundColor
 import com.example.myfitnessapp.ui.color.textColor
 
 @Composable
-fun DaysOfWeekView(daysOfWeek: Set<WeekDay>) {
-    Box {
+fun DaysOfWeekView(
+    daysOfWeek: Set<DayOfWeek>,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier.fillMaxWidth()
+    ) {
         val daysOfWeekArray = arrayOf(false, false, false, false, false, false, false)
         daysOfWeek.forEach { day ->
             daysOfWeekArray[day.ordinal] = true
         }
-
-        Row {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
             daysOfWeekArray.forEachIndexed { index, isDaySelected ->
                 Text(
-                    WeekDay.entries[index].value,
+                    DayOfWeek.entries[index].value,
                     fontSize = 10.sp,
                     color = if (isDaySelected) backgroundColor else textColor,
                     modifier = Modifier
