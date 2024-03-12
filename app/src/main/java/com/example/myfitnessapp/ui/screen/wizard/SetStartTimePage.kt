43a598731d1d9e06bfa6ac4ff5e0dc73204b0b42
swksysb114 <swksysb1124@gmail.com>
2024-03-12 22:23:58 +0800

    feat: implement DaysOfWeekView to set and Text to describe selected days of week

--- app/src/main/java/com/example/myfitnessapp/ui/screen/wizard/SetStartTimePage.kt
+++ app/src/main/java/com/example/myfitnessapp/ui/screen/wizard/SetStartTimePage.kt
@@ -1,11 +1,23 @@
 package com.example.myfitnessapp.ui.screen.wizard
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.DateRange
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myfitnessapp.ui.color.buttonBackgroundColor
+import com.example.myfitnessapp.ui.color.textColor
+import com.example.myfitnessapp.ui.component.DaysOfWeekView
 import com.example.myfitnessapp.ui.component.SettingWizardLayout
 import com.example.myfitnessapp.ui.theme.MyFitnessAppTheme
 
@@ -22,17 +34,45 @@ fun SetStartTimePage(
         onNext = onNext,
         nextEnabled = startTime.isNotEmpty()
     ) {
-        WizardTextField(
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .padding(
-                    horizontal = 20.dp,
-                    vertical = 12.dp
-                ),
-            label = "訓練時間 (小時:分鐘)",
-            value = startTime,
-            onValueChange = onStartTimeChange
-        )
+        Column(verticalArrangement = Arrangement.spacedBy(20.dp)) {
+            WizardTextField(
+                modifier = Modifier
+                    .padding(
+                        horizontal = 20.dp,
+                        vertical = 12.dp
+                    ),
+                label = "訓練時間 (小時:分鐘)",
+                value = startTime,
+                onValueChange = onStartTimeChange
+            )
+            Column {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            horizontal = 20.dp,
+                            vertical = 12.dp
+                        ),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Icon(
+                        tint = buttonBackgroundColor,
+                        imageVector = Icons.Sharp.DateRange,
+                        contentDescription = null)
+                    Text("每天", color = textColor, fontSize = 18.sp)
+                }
+                DaysOfWeekView(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.CenterHorizontally)
+                        .padding(
+                            horizontal = 20.dp,
+                            vertical = 12.dp
+                        ),
+                    daysOfWeek = setOf()
+                )
+            }
+        }
     }
 }
 
