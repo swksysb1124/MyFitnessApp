a009a4cd49d29ecc2b5b4e328741f21d2fb76acc
swksysb114 <swksysb1124@gmail.com>
2024-03-14 08:50:09 +0800

    feat: add @ColumnInfo in LessonEntity, ExerciseEntity

--- app/src/main/java/com/example/myfitnessapp/database/ExerciseEntity.kt
+++ app/src/main/java/com/example/myfitnessapp/database/ExerciseEntity.kt
@@ -1,13 +1,13 @@
 package com.example.myfitnessapp.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "exercise")
 class ExerciseEntity(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int? = null,
-    val type: String,
-    val name: String,
-    val period: Int
+    @PrimaryKey(autoGenerate = true) val id: Int? = null,
+    @ColumnInfo(name = "type") val type: String,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "duration") val duration: Int
 )
