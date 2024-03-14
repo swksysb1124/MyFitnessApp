d3239d13dfdc59790e387af3e89499784f4404ca
swksysb114 <swksysb1124@gmail.com>
2024-03-14 19:39:51 +0800

    feat: add lessonId as foreign key in ExerciseEntity and remove LessonExerciseEntity.kt

--- app/src/main/java/com/example/myfitnessapp/database/LessonExerciseEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myfitnessapp.database
-
-import androidx.room.Entity
-
-@Entity(tableName = "lesson_exercise")
-data class LessonExerciseEntity(
-    val lessonId: Int,
-    val exerciseId: Int
-)
