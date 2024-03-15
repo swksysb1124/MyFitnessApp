ca59c14959bc831f07601757eb163b35045cd168
swksysb114 <swksysb1124@gmail.com>
2024-03-15 08:15:24 +0800

    fix: handle unspecified case for serialization of List<DayOfWeek>

--- app/src/main/java/com/example/myfitnessapp/serialization/DatabaseSerialzation.kt
+++ app/src/main/java/com/example/myfitnessapp/serialization/DatabaseSerialzation.kt
@@ -4,6 +4,7 @@ import com.example.myfitnessapp.model.DayOfWeek
 
 // format: SUN:MON:TUE:WED:THU:FRI:SAT
 fun String.deserializeDaysOfWeek(): List<DayOfWeek> {
+    if (isEmpty()) return emptyList()
     val days = this.split(":")
     return days.map { day ->
         DayOfWeek.entries.first { daysOfWeek -> daysOfWeek.name == day }
@@ -12,6 +13,7 @@ fun String.deserializeDaysOfWeek(): List<DayOfWeek> {
 
 // format: SUN:MON:TUE:WED:THU:FRI:SAT
 fun List<DayOfWeek>.serializeDaysOfWeek(): String {
+    if (isEmpty()) return ""
     return this.joinToString(":") {
         it.name
     }
