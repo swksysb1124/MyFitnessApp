ba943de0edbe1852cbdba9d949e72e353ca0ec84
swksysb114 <swksysb1124@gmail.com>
2024-03-16 12:53:01 +0800

    feat: add suggestedDurationInSecond in ExerciseMetaData

--- app/src/main/java/com/example/myfitnessapp/model/ExerciseMetaData.kt
+++ app/src/main/java/com/example/myfitnessapp/model/ExerciseMetaData.kt
@@ -8,39 +8,96 @@ sealed class ExerciseMetaData(
     val key: String,
     val name: String,
     @DrawableRes val icon: Int,
-    val mets: Mets
+    val mets: Mets,
+    val suggestedDurationInSecond: Int
 ) {
     data object LowStrengthRunning : ExerciseMetaData(
         key = "low_strength_running",
         name = "低強度跑步",
         icon = R.drawable.run,
-        mets = Mets(8.0f)
+        mets = Mets(8.0f),
+        suggestedDurationInSecond = 20 * 60
     )
 
     data object MediumStrengthRunning : ExerciseMetaData(
         key = "medium_strength_running",
         name = "中強度跑步",
         icon = R.drawable.run,
-        mets = Mets(11.5f)
+        mets = Mets(11.5f),
+        suggestedDurationInSecond = 45 * 60
     )
 
     data object HighStrengthRunning : ExerciseMetaData(
         key = "high_strength_running",
         name = "高強度跑步",
         icon = R.drawable.run,
-        mets = Mets(16.0f)
+        mets = Mets(16.0f),
+        suggestedDurationInSecond = 60 * 60
     )
 
-    data object BrisklyWalking :
-        ExerciseMetaData("briskly_walking", "快走", R.drawable.briskly_walking, Mets(4.5f))
+    data object BrisklyWalking : ExerciseMetaData(
+        key = "briskly_walking",
+        name = "快走",
+        icon = R.drawable.briskly_walking,
+        mets = Mets(4.5f),
+        suggestedDurationInSecond = 20 * 60
+    )
 
-    data object Swimming : ExerciseMetaData("swimming", "游泳", R.drawable.swimming, Mets(7.0f))
-    data object PushUp : ExerciseMetaData("push_up", "伏地挺身", R.drawable.push_up, Mets(3.8f))
-    data object PullUp : ExerciseMetaData("pull_up", "引體向上", R.drawable.pull_up, Mets(3.8f))
-    data object Squat : ExerciseMetaData("squat", "深蹲", R.drawable.squat, Mets(3.8f))
-    data object Plank : ExerciseMetaData("plank", "棒式", R.drawable.plank, Mets(3.8f))
-    data object Crunch : ExerciseMetaData("crunch", "捲腹", R.drawable.crunch, Mets(3.8f))
-    data object Bridge : ExerciseMetaData("bridge", "橋式", R.drawable.bridge, Mets(3.8f))
+    data object Swimming : ExerciseMetaData(
+        key = "swimming",
+        name = "游泳",
+        icon = R.drawable.swimming,
+        mets = Mets(7.0f),
+        suggestedDurationInSecond = 60 * 60
+    )
+
+    data object PushUp : ExerciseMetaData(
+        key = "push_up",
+        name = "伏地挺身",
+        icon = R.drawable.push_up,
+        mets = Mets(3.8f),
+        suggestedDurationInSecond = 30
+    )
+
+    data object PullUp : ExerciseMetaData(
+        key = "pull_up",
+        name = "引體向上",
+        icon = R.drawable.pull_up,
+        mets = Mets(3.8f),
+        suggestedDurationInSecond = 30
+    )
+
+    data object Squat : ExerciseMetaData(
+        key = "squat",
+        name = "深蹲",
+        icon = R.drawable.squat,
+        mets = Mets(3.8f),
+        suggestedDurationInSecond = 30
+    )
+
+    data object Plank : ExerciseMetaData(
+        key = "plank",
+        name = "棒式",
+        icon = R.drawable.plank,
+        mets = Mets(3.8f),
+        suggestedDurationInSecond = 30
+    )
+
+    data object Crunch : ExerciseMetaData(
+        key = "crunch",
+        name = "捲腹",
+        icon = R.drawable.crunch,
+        mets = Mets(3.8f),
+        suggestedDurationInSecond = 30
+    )
+
+    data object Bridge : ExerciseMetaData(
+        key = "bridge",
+        name = "橋式",
+        icon = R.drawable.bridge,
+        mets = Mets(3.8f),
+        suggestedDurationInSecond = 30
+    )
 
     companion object {
         val All = listOf(
