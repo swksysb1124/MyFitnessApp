54614a8a18c66299053fb8ebe7f712db5c6b0ad9
swksysb114 <swksysb1124@gmail.com>
2024-03-16 23:31:31 +0800

    feat: refine LessonRow

--- app/src/main/java/com/example/myfitnessapp/ui/screen/lession/LessonRow.kt
+++ app/src/main/java/com/example/myfitnessapp/ui/screen/lession/LessonRow.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
@@ -15,11 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myfitnessapp.model.DayOfWeek
-import com.example.myfitnessapp.ui.color.backgroundColor
 import com.example.myfitnessapp.ui.color.rowBackgroundColor
 import com.example.myfitnessapp.ui.color.textColor
 import com.example.myfitnessapp.ui.theme.MyFitnessAppTheme
@@ -44,26 +46,46 @@ fun LessonRow(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
+        Row(
+            modifier = Modifier.weight(1f),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             if (screenMode == LessonScreenMode.Edit) {
                 Checkbox(
                     checked = checked,
                     onCheckedChange = onCheckedChange
                 )
             }
-            Column {
+            Column(Modifier.fillMaxHeight(), verticalArrangement = Arrangement.SpaceBetween) {
                 if (name != null) {
-                    Text(text = name, fontSize = 16.sp, color = backgroundColor)
+                    Text(
+                        text = name,
+                        fontSize = 24.sp,
+                        color = textColor,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
                 }
-                Spacer(modifier = Modifier.height(5.dp))
-                Text(startTime, fontSize = 30.sp, color = textColor, textAlign = TextAlign.Center)
-                Spacer(modifier = Modifier.height(5.dp))
                 Text(duration, fontSize = 16.sp, color = textColor)
+                val daysOfWeekDes = DayOfWeek.generateWeekDescription(daysOfWeek)
+                Text(
+                    daysOfWeekDes,
+                    color = textColor,
+                    fontSize = 14.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
             }
         }
-
-        val daysOfWeekDes = DayOfWeek.generateWeekDescription(daysOfWeek)
-        Text(daysOfWeekDes, color = textColor, fontSize = 18.sp)
+        Spacer(modifier = Modifier.width(10.dp))
+        Column(
+            modifier = Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.End
+        ) {
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(startTime, fontSize = 24.sp, color = textColor, textAlign = TextAlign.Center)
+        }
     }
 }
 
@@ -75,8 +97,15 @@ fun LessonRowPreview() {
             screenMode = LessonScreenMode.Edit,
             name = "晨間運動",
             startTime = "18:00",
-            duration = "一小時",
-            daysOfWeek = listOf(DayOfWeek.SAT, DayOfWeek.SUN, DayOfWeek.MON)
+            duration = "10小時59分59秒",
+            daysOfWeek = listOf(
+                DayOfWeek.SAT,
+                DayOfWeek.SUN,
+                DayOfWeek.MON,
+                DayOfWeek.TUE,
+                DayOfWeek.WED,
+                DayOfWeek.FRI
+            )
         )
     }
 }
