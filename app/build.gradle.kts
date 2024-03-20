6acdc7bf2a6788aae04dac8166873325ff86a4b4
swksysb114 <swksysb1124@gmail.com>
2024-03-20 23:57:45 +0800

    feat: update versionCode to 2 for production release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "studio.jasonsu.myfitness"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
