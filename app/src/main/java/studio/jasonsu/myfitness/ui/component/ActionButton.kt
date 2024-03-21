c8f1c0a0a434597e99749631081bb87c52f8ba58
swksysb114 <swksysb1124@gmail.com>
2024-03-21 20:20:05 +0800

    fix: address the low contrast issue of ActionButton - add preview of ActionButton

--- app/src/main/java/studio/jasonsu/myfitness/ui/component/ActionButton.kt
+++ app/src/main/java/studio/jasonsu/myfitness/ui/component/ActionButton.kt
@@ -1,15 +1,16 @@
 package studio.jasonsu.myfitness.ui.component
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import studio.jasonsu.myfitness.ui.theme.MyFitnessAppTheme
 
 @Composable
 fun ActionButton(
@@ -19,9 +20,6 @@ fun ActionButton(
     onAction: () -> Unit
 ) {
     Button(
-        colors = ButtonDefaults.buttonColors(
-            containerColor = MaterialTheme.colorScheme.primary
-        ),
         enabled = enabled,
         shape = RoundedCornerShape(20.dp),
         modifier = modifier,
@@ -30,8 +28,28 @@ fun ActionButton(
         Text(
             text = actionName,
             fontSize = 20.sp,
-            color = MaterialTheme.colorScheme.onPrimary,
             modifier = Modifier.padding(vertical = 10.dp)
         )
+    }
+}
+
+@Preview(
+    apiLevel = 33,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(
+    backgroundColor = 0xFFFFFFFF,
+    showBackground = true,
+    apiLevel = 33,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight"
+)
+@Composable
+fun ActionButtonPreview() {
+    MyFitnessAppTheme {
+        ActionButton(actionName = "開始", enabled = false) {
+
+        }
     }
 }
