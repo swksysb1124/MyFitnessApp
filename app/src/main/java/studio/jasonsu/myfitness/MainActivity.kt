b51d36c73f4be2d5cba000d9884998db203c54c4
swksysb114 <swksysb1124@gmail.com>
2024-03-23 21:55:05 +0800

    feat: extract MyFitnessApp, set notification channel, and set alarm for testing

--- app/src/main/java/studio/jasonsu/myfitness/MainActivity.kt
+++ app/src/main/java/studio/jasonsu/myfitness/MainActivity.kt
@@ -1,47 +1,27 @@
 package studio.jasonsu.myfitness
 
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import androidx.room.Room
+import studio.jasonsu.myfitness.app.MyFitnessApp
 import studio.jasonsu.myfitness.database.AppDatabase
 import studio.jasonsu.myfitness.database.exercise.ExerciseDao
 import studio.jasonsu.myfitness.database.lesson.LessonDao
 import studio.jasonsu.myfitness.database.profile.ProfileDao
 import studio.jasonsu.myfitness.datasource.DatabaseLessonDataSource
 import studio.jasonsu.myfitness.datasource.LocalLessonDataSource
-import studio.jasonsu.myfitness.event.RefreshLessonListEvent
-import studio.jasonsu.myfitness.navigation.NaviScreen
-import studio.jasonsu.myfitness.navigation.WizardNaviPage
+import studio.jasonsu.myfitness.repository.LessonAlarmRepository
 import studio.jasonsu.myfitness.repository.LessonExerciseRepository
 import studio.jasonsu.myfitness.repository.LessonRepository
 import studio.jasonsu.myfitness.repository.PersistProfileRepository
 import studio.jasonsu.myfitness.repository.ProfileRepository
-import studio.jasonsu.myfitness.ui.screen.lession.LessonContentScreen
-import studio.jasonsu.myfitness.ui.screen.lession.LessonContentViewModel
-import studio.jasonsu.myfitness.ui.screen.lession.LessonExercisePage
-import studio.jasonsu.myfitness.ui.screen.lession.LessonExerciseViewModel
-import studio.jasonsu.myfitness.ui.screen.main.MainScreen
 import studio.jasonsu.myfitness.ui.screen.main.MainViewModel
-import studio.jasonsu.myfitness.ui.screen.wizard.add.lesson.AddLessonScreen
-import studio.jasonsu.myfitness.ui.screen.wizard.add.lesson.AddLessonViewModel
-import studio.jasonsu.myfitness.ui.screen.wizard.add.profile.AddProfileScreen
-import studio.jasonsu.myfitness.ui.screen.wizard.add.profile.AddProfileViewModel
-import studio.jasonsu.myfitness.ui.theme.MyFitnessAppTheme
-import studio.jasonsu.myfitness.util.animation.leftSlideInNaviAnimation
-import studio.jasonsu.myfitness.util.animation.leftSlideOutNaviAnimation
-import studio.jasonsu.myfitness.util.animation.rightSlideInNaviAnimation
-import studio.jasonsu.myfitness.util.animation.rightSlideOutNaviAnimation
+import studio.jasonsu.myfitness.util.NotificationUtil
 import studio.jasonsu.myfitness.util.tts.TextToSpeakUtil
 
 class MainActivity : ComponentActivity() {
@@ -57,6 +37,7 @@ class MainActivity : ComponentActivity() {
     private val lessonExerciseRepository: LessonExerciseRepository by lazy { LessonExerciseRepository(dataSource) }
     private val lessonRepository: LessonRepository by lazy { LessonRepository(dataSource) }
     private lateinit var profileRepository: ProfileRepository
+    private val lessonAlarmRepository = LessonAlarmRepository()
 
     // main view model for sharing state between screen
     private lateinit var mainViewModel: MainViewModel
@@ -72,150 +53,28 @@ class MainActivity : ComponentActivity() {
             splashScreen.setKeepOnScreenCondition { !isReady }
         }
 
+        setLessonNotificationChannel(this)
+        lessonAlarmRepository.setAlarm(this)
         setContent {
-            MyFitnessAppTheme {
-                val mainNavController = rememberNavController()
-                NavHost(
-                    navController = mainNavController,
-                    startDestination = NaviScreen.Main.route,
-                    enterTransition = { EnterTransition.None },
-                    exitTransition = { ExitTransition.None }
-                ) {
-                    composable(NaviScreen.AddProfile.route) {
-                        AddProfileScreen(
-                            viewModel = viewModel {
-                                AddProfileViewModel(profileRepository)
-                            },
-                            onConfirm = mainNavController::popBackStack,
-                            onFinished = ::finish
-                        )
-                    }
-                    composable(
-                        route = NaviScreen.Main.route,
-                        enterTransition = {
-                            when (initialState.destination.route) {
-                                NaviScreen.Lesson.route -> rightSlideInNaviAnimation()
-                                else -> null
-                            }
-                        },
-                        exitTransition = {
-                            when (targetState.destination.route) {
-                                NaviScreen.Lesson.route -> leftSlideOutNaviAnimation()
-                                else -> null
-                            }
-                        }
-                    ) {
-                        MainScreen(
-                            mainViewModel = mainViewModel,
-                            lessonRepository = lessonRepository,
-                            profileRepository = profileRepository,
-                            onLessonClick = { id ->
-                                mainNavController.navigate(
-                                    NaviScreen.Lesson.createNaviRoute(id)
-                                )
-                            },
-                            onAddLesson = {
-                                mainNavController.navigate(WizardNaviPage.AddLessonMain.route)
-                            },
-                            onAddProfile = {
-                                mainNavController.navigate(NaviScreen.AddProfile.route)
-                            },
-                            onFinished = ::finish
-                        )
-                    }
-                    composable(
-                        route = NaviScreen.Lesson.route,
-                        arguments = listOf(
-                            navArgument(NaviScreen.LESSON_ID) { type = NavType.StringType }
-                        ),
-                        enterTransition = {
-                            when (initialState.destination.route) {
-                                NaviScreen.Main.route -> leftSlideInNaviAnimation()
-                                NaviScreen.LessonExercise.route -> rightSlideInNaviAnimation()
-                                else -> null
-                            }
-                        },
-                        exitTransition = {
-                            when (targetState.destination.route) {
-                                NaviScreen.Main.route -> rightSlideOutNaviAnimation()
-                                NaviScreen.LessonExercise.route -> leftSlideOutNaviAnimation()
-                                else -> null
-                            }
-                        }
-                    ) {
-                        val lessonId = it.arguments?.getString(NaviScreen.LESSON_ID)
-                        LessonContentScreen(
-                            viewModel = viewModel(
-                                key = lessonId
-                            ) {
-                                LessonContentViewModel(
-                                    id = lessonId,
-                                    lessonRepository = lessonRepository,
-                                    lessonExerciseRepository = lessonExerciseRepository,
-                                    profileRepository = profileRepository
-                                )
-                            },
-                            onStartButtonClick = {
-                                mainNavController.navigate(
-                                    NaviScreen.LessonExercise.createNaviRoute(
-                                        lessonId ?: ""
-                                    )
-                                )
-                            },
-                            onBackPressed = mainNavController::popBackStack
-                        )
-                    }
-                    composable(
-                        route = NaviScreen.LessonExercise.route,
-                        arguments = listOf(
-                            navArgument(NaviScreen.LESSON_ID) { type = NavType.StringType }
-                        ),
-                        enterTransition = {
-                            when (initialState.destination.route) {
-                                NaviScreen.Lesson.route -> leftSlideInNaviAnimation()
-                                else -> null
-                            }
-                        },
-                        exitTransition = {
-                            when (targetState.destination.route) {
-                                NaviScreen.Lesson.route -> rightSlideOutNaviAnimation()
-                                else -> null
-                            }
-                        }
-                    ) {
-                        val lessonId = it.arguments?.getString(NaviScreen.LESSON_ID)
-                        LessonExercisePage(
-                            viewModel = viewModel(
-                                key = lessonId
-                            ) {
-                                LessonExerciseViewModel(
-                                    lessonId = lessonId,
-                                    textToSpeech = textToSpeech,
-                                    lessonExerciseRepository = lessonExerciseRepository
-                                )
-                            },
-                            onBackPressed = mainNavController::popBackStack
-                        )
-                    }
-
-                    composable(WizardNaviPage.AddLessonMain.route) {
-                        AddLessonScreen(
-                            viewModel = viewModel {
-                                AddLessonViewModel(
-                                    lessonRepository = lessonRepository,
-                                    lessonExerciseRepository = lessonExerciseRepository
-                                )
-                            },
-                            onDismiss = mainNavController::popBackStack,
-                            onWizardSettingComplete = {
-                                mainNavController.popBackStack()
-                                mainViewModel.sentEvent(RefreshLessonListEvent)
-                            }
-                        )
-                    }
-                }
-            }
+            MyFitnessApp(
+                profileRepository = profileRepository,
+                lessonRepository = lessonRepository,
+                lessonExerciseRepository = lessonExerciseRepository,
+                textToSpeech = textToSpeech,
+                mainViewModel = mainViewModel,
+                onFinished = ::finish
+            )
         }
+    }
+
+    private fun setLessonNotificationChannel(context: Context) {
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        NotificationUtil.createNotificationChannel(
+            manager = notificationManager,
+            channelId = NotificationUtil.LESSON_ALARM_CHANNEL_ID,
+            channelName = NotificationUtil.LESSON_START_CHANNEL_NAME
+        )
     }
 
     private fun initializeDataSource() {
