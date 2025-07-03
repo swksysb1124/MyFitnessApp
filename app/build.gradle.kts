d16ad21c59f96463868bccdd8181eb5ab6a3defe
swksysb114 <swksysb1124@gmail.com>
2025-07-03 22:38:21 +0800

    chore: set versionName 1.1.0 & versionCode 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "studio.jasonsu.myfitness"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
