4bfd7d7cb2c0f87dd0f8df7bb617138720c4bd43
swksysb1124 <swksysb1124@gmail.com>
2025-07-12 15:12:21 +0800

    Merge main back to develop after release (#20)
    chore: update CHANGELOG.md

--- app/src/main/java/studio/jasonsu/myfitness/ui/screen/lession/MyLessonScreen.kt
+++ app/src/main/java/studio/jasonsu/myfitness/ui/screen/lession/MyLessonScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
--- app/src/main/java/studio/jasonsu/myfitness/ui/screen/wizard/add/lesson/LessonSettingConfirmPage.kt
+++ app/src/main/java/studio/jasonsu/myfitness/ui/screen/wizard/add/lesson/LessonSettingConfirmPage.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
